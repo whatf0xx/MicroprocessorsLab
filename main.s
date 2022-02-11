a2ec8d0d92eb580b484a4a662adc307b83f74b32
Fox <whatf0xx@gmail.com>
2022-02-11 11:49:13 +0000

    Implemented a delay

--- main.s
+++ main.s
@@ -15,14 +15,30 @@ start:
 	movlw	0x0		    ; Reset W low
 	bra 	test
 loop:
-	movff 	0x00, PORTC	    ; 0x00 is the counter
+	call	init_delay
+        movff 	0x00, PORTC	    ; 0x00 is the counter
 	movff   PORTD, 0x01	    ; 0x01 is how much to iterate by
-	movf	0x01, W		    ; send 0x01(iteration)to W
+	movf	0x01, 0, 0	    ; send 0x01(iteration)to W
 	addwf 	0x00, A		    ; Add W (iteration) to 0x00 (counter)
 test:
 	movlw 	0xff
 	cpfsgt 	0x00, A
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
-
+	
+	goto finish
+init_delay:
+	movlw 0xff
+	movwf 0x02, A
+outer_delay:
+	movlw 0xff
+	movwf 0x03, A
+	decfsz 0x02, F, A
+	goto inner_delay
+	return
+inner_delay:
+	decfsz 0x03, F, A
+	bra inner_delay
+	bra outer_delay
+finish:
 	end	main
