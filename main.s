8b429975413a046e38f399187ff4f4ca9647680a
Fox <whatf0xx@gmail.com>
2022-02-11 16:31:30 +0000

    My memory test function works!

--- main.s
+++ main.s
@@ -14,3 +14,30 @@ loop:
 	decfsz length, A	;decrease the counter, if good continue
 	goto loop
 	
+	movlw 0xa
+	movwf length, A
+	lfsr 1, start		;set FSR1 to the origin
+	
+loop2:	
+	movf INDF1, W, A	;take the current value of the array to W
+	cpfseq POSTINC1, A	;Skip if value matches position
+	goto failure
+	decfsz length, A	;decrease the counter, if good continue
+	goto loop2
+	goto success
+	
+failure:
+	movlw 0x00
+	movwf TRISJ, A
+	movlw 0xff
+	movwf PORTJ, A		;FAILURE LIGHTS UP PORT J
+	goto finish
+	
+success:
+	movlw 0x00
+	movwf TRISH, A
+	movlw 0xff
+	movwf PORTH, A		;FAILURE LIGHTS UP PORT H
+	
+finish:
+	end
